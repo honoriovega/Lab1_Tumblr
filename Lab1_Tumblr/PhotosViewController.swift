01059c2cb9555fb1c8ba3577495f6f0597ad7c60
Honorio Vega <hvega@csumb.edu>
2018-02-01 11:10:18 -0800

    Optional 1: Add Pull to Refresh - Completed

--- Lab1_Tumblr/PhotosViewController.swift
+++ Lab1_Tumblr/PhotosViewController.swift
@@ -12,14 +12,30 @@ import AlamofireImage
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var posts : [[String : Any]] = []
+    var refreshControl : UIRefreshControl!
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+         super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(PhotosViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        fetchPictures()
+
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl : UIRefreshControl) {
+        fetchPictures()
+    }
+    
+    func fetchPictures() {
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -32,17 +48,16 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                 //print(dataDictionary)
                 
-                // TODO: Get the posts and store in posts property
                 // Get the dictionary from the response key
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
                 
             }
         }
         task.resume()
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -77,4 +92,5 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         }
         return cell
     }
+
 }
