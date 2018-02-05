cddaf799c84b6ae741cbd67b36ff671a94dfb13a
Honorio Vega <hvega@csumb.edu>
2018-02-04 17:31:00 -0800

    update

--- Pods/Target Support Files/Pods-Lab1_TumblrTests/Pods-Lab1_TumblrTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lab1_TumblrTests : NSObject
-@end
-@implementation PodsDummy_Pods_Lab1_TumblrTests
-@end
--- Pods/Target Support Files/Pods-Lab1_TumblrUITests/Pods-Lab1_TumblrUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lab1_TumblrUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Lab1_TumblrUITests
-@end
